5ec71ebb0d4fb5e4a7dee9967a1da5a28aeaa944
David. J <codewithjalloh@gmail.com>
2020-05-09 13:52:32 +0100

    materialApp, Scaffold and appBar added

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,13 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.grey,
+
+        ),
+      ),
+    );
   }
 }
