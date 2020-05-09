9b572c0764ac5cc7e1c6fd37fb2179183d63a5fb
David. J <codewithjalloh@gmail.com>
2020-05-09 14:21:16 +0100

    center body, column with text and image widget added

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,24 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.grey,
+          title: Text('Imhotep'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text('EGYPTIAN ARCHITECT, PHYSICIAN, AND STATESMAN'),
+              Container(
+                child: Image(
+                  image: AssetImage('images/Imhotep.jpg'),
+                ),
+              ),
+              Text('Imhotep, Greek Imouthes, (born 27th century BCE, Memphis, Egypt), vizier, sage, architect, astrologer, and chief minister to Djoser (reigned 2630–2611 BCE), the second king of Egypt’s third dynasty, who was later worshipped as the god of medicine in Egypt and in Greece, where he was identified with the Greek god of medicine, Asclepius. He is considered to have been the architect of the step pyramid built at the necropolis of Ṣaqqārah in the city of Memphis. The oldest extant monument of hewn stone known to the world, the pyramid consists of six steps and attains a height of 200 feet (61 metres).')
+
+            ],
+          ),
+        ),
 
         ),
-      ),
-    );
+      );
   }
 }
